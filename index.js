8c55d329f594f4573360100cb7976a9f82f13cf9
Realmy <43610767+ItsRealmy@users.noreply.github.com>
2021-07-26 22:28:12 +0200

    Switch to bing

--- index.js
+++ index.js
@@ -14,103 +14,67 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/search", async (req, res) => {
-	if (req.query.password == "aVLhgkpS8X4Gb9Cp") {
-		var query = req.query.q;
+    if (req.query.password == "aVLhgkpS8X4Gb9Cp") {
+        var query = req.query.q;
 
-    var response = await axios.get(`https://html.duckduckgo.com/html?q=${query}`)
-        .then(function (response) {
-            return response.data;
-        })
-        .catch(function (error) {
-            console.log(error);
-            return false;
-        })
-
-    if (response) {
-        var document = parser.parse(response);
-
-        var DDGresults = document.querySelectorAll(".results > .result > .links_main");
-        var results = parseEntries(DDGresults);
-
-        var formData = document.querySelector(".results > .nav-link > form").innerHTML;
-        formData = parseFormData(formData);
-
-        var toURL = "https://html.duckduckgo.com/html";
-
-        var ii = 0;
-        for (var i in formData) {
-            ii++;
-            var item = formData[i];
-
-            if (ii == 1) {
-                toURL = toURL + `?${i}=${item}`;
-            } else {
-                toURL = toURL + `&${i}=${item}`;
-            }
-        }
-
-        var response2 = await axios.get(toURL)
+        var response = await axios.get(`https://bing.com/search?q=${query}`)
             .then(function (response) {
                 return response.data;
             })
             .catch(function (error) {
+                console.log(error);
                 return false;
-            });
+            })
 
-        if (response2) {
+        var response2 = await axios.get(`https://bing.com/search?q=${query}&first=7`)
+            .then(function (response) {
+                return response.data;
+            })
+            .catch(function (error) {
+                console.log(error);
+                return false;
+            })
+
+        if (response && response2) {
+            var document = parser.parse(response);
+
+            var results = document.querySelectorAll("#b_results > .b_algo");
+
+            var resultsArray = [];
+
+            for (var i in results) {
+                var result = results[i];
+                result = parser.parse(`<div class="qdex-result">${result.innerHTML}</div>`);
+
+                var obj = {};
+                obj.title = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").innerHTML : "";
+                obj.url = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").rawAttributes.href : "";
+                obj.description = (result.querySelector(".b_caption > p")) ? result.querySelector("h2 > a").innerHTML : "";
+
+                resultsArray.push(obj);
+            }
+
             var document2 = parser.parse(response2);
 
-            var DDGresults2 = document2.querySelectorAll(".results > .result > .links_main");
-            var results2 = parseEntries(DDGresults2);
+            var results2 = document2.querySelectorAll("#b_results > .b_algo");
 
-            results = results.concat(results2);
+            var resultsArray2 = [];
+
+            for (var i in results2) {
+                var result = results2[i];
+                result = parser.parse(`<div class="qdex-result">${result.innerHTML}</div>`);
+
+                var obj = {};
+                obj.title = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").innerHTML.split("<strong>").join("").split("</strong>").join("") : "";
+                obj.url = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").rawAttributes.href : "";
+                obj.description = (result.querySelector(".b_caption > p")) ? result.querySelector("h2 > a").innerHTML.split("<strong>").join("").split("</strong>").join("") : "";
+
+                resultsArray2.push(obj);
+            }
+
+            resultsArray = resultsArray.concat(resultsArray2);
+
+            res.send({ success: true, results: resultsArray });
         }
-
-        var responseObj = {
-            success: true,
-            results: results
-        }
-
-        res.header("Content-Type", "application/json");
-        res.send(JSON.stringify(responseObj, null, 4));
-    } else {
-        res.send({ success: false, code: "AXIOS_ERROR" });
     }
-	}
 });
-
-function parseFormData(form) {
-    var document = parser.parse(form);
-    var inputs = document.querySelectorAll("input");
-
-    var obj = {};
-
-    for (var i in inputs) {
-        var input = inputs[i];
-
-        if (input.rawAttributes.name) {
-            obj[input.rawAttributes.name] = input.rawAttributes.value;
-        }
-    }
-
-    return obj;
-}
-
-function parseEntries(DDGresults) {
-    var results = [];
-
-    for (var i in DDGresults) {
-        var item = DDGresults[i];
-
-        item = parser.parse("<div class='qscrape-body'>" + item.innerHTML + "</div>");
-
-        var obj = {};
-        obj.title = item.querySelector(".qscrape-body > h2 > a").innerHTML;
-        obj.url = decode(item.querySelector(".result__a").rawAttributes.href.split("//duckduckgo.com/l/?uddg=")[1].split("&amp;rut=")[0]);
-        obj.description = (item.querySelector(".qscrape-body > .result__snippet")) ? item.querySelector(".qscrape-body > .result__snippet").innerHTML : "";
-
-        results.push(obj);
-    }
-
-    return results;
-}
