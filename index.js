857167a546847028b7f7fe48a5a747128382a53c
emilinadaniel27 <52750024+emilinadaniel27@users.noreply.github.com>
2021-07-27 18:03:42 +0200

    Open source aaaa

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/search", async (req, res) => {
-    if (req.query.password == "aVLhgkpS8X4Gb9Cp") {
+    if (req.query.password == "aVLhgkpS8X4Gb9Cp" && req.query.q) {
         var query = req.query.q;
+
+        //&setmkt=en-us
 
         var response = await axios.get(`https://bing.com/search?q=${query}`)
             .then(function (response) {
@@ -26,17 +28,29 @@ app.get("/search", async (req, res) => {
                 return false;
             })
 
-        var response2 = await axios.get(`https://bing.com/search?q=${query}&first=7`)
-            .then(function (response) {
-                return response.data;
-            })
-            .catch(function (error) {
-                console.log(error);
-                return false;
-            })
-
-        if (response && response2) {
+        if (response) {
             var document = parser.parse(response);
+
+            var link2 = (document.querySelectorAll(`.b_pag a`) !== 0) ? document.querySelectorAll(`.b_pag a`) : false;
+
+            if (link2) {
+                link2.forEach((element) => {
+                    if (element.innerHTML == "2") {
+                        link2 = element.rawAttributes.href;
+                    }
+                })
+
+                var response2 = await axios.get(`https://www.bing.com${link2}`)
+                .then(function (response) {
+                    return response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    return false;
+                })
+
+                console.log(`https://www.bing.com${link2}`)
+            }
 
             var results = document.querySelectorAll("#b_results > .b_algo");
 
@@ -47,29 +61,31 @@ app.get("/search", async (req, res) => {
                 result = parser.parse(`<div class="qdex-result">${result.innerHTML}</div>`);
 
                 var obj = {};
-                obj.title = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").innerHTML : "";
-                obj.url = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").rawAttributes.href : "";
-                obj.description = (result.querySelector(".b_caption > p")) ? result.querySelector("h2 > a").innerHTML : "";
-
-                resultsArray.push(obj);
-            }
-
-            var document2 = parser.parse(response2);
-
-            var results2 = document2.querySelectorAll("#b_results > .b_algo");
-
-            var resultsArray2 = [];
-
-            for (var i in results2) {
-                var result = results2[i];
-                result = parser.parse(`<div class="qdex-result">${result.innerHTML}</div>`);
-
-                var obj = {};
                 obj.title = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").innerHTML.split("<strong>").join("").split("</strong>").join("") : "";
                 obj.url = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").rawAttributes.href : "";
                 obj.description = (result.querySelector(".b_caption > p")) ? result.querySelector("h2 > a").innerHTML.split("<strong>").join("").split("</strong>").join("") : "";
 
-                resultsArray2.push(obj);
+                resultsArray.push(obj);
+            }
+
+            if (response2) {
+                var document2 = parser.parse(response2);
+
+                var results2 = document2.querySelectorAll("#b_results > .b_algo");
+
+                var resultsArray2 = [];
+
+                for (var i in results2) {
+                    var result = results2[i];
+                    result = parser.parse(`<div class="qdex-result">${result.innerHTML}</div>`);
+
+                    var obj = {};
+                    obj.title = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").innerHTML.split("<strong>").join("").split("</strong>").join("") : "";
+                    obj.url = (result.querySelector("h2 > a")) ? result.querySelector("h2 > a").rawAttributes.href : "";
+                    obj.description = (result.querySelector(".b_caption > p")) ? result.querySelector("h2 > a").innerHTML.split("<strong>").join("").split("</strong>").join("") : "";
+
+                    resultsArray2.push(obj);
+                }
             }
 
             resultsArray = resultsArray.concat(resultsArray2);
