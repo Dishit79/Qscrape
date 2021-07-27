836cc58677186d33120e67a0f8251bcfb683dd1a
Dishit79 <Dishit79@gmail.com>
2021-07-27 13:34:29 -0400

    Scraping of second page implamented

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ app.get("/search", async (req, res) => {
                     }
                 })
 
-                var response2 = await axios.get(`https://www.bing.com${link2}`)
+                let linknum = link2.slice(-23).replace(/[^0-9]/g, "") // gets the link number for second page 
+                var response2 = await axios.get(`https://www.bing.com${link2}&first=${linknum}&FORM=PERE`)
                 .then(function (response) {
                     return response.data;
                 })
@@ -50,8 +51,8 @@ app.get("/search", async (req, res) => {
                     console.log(error);
                     return false;
                 })
+                              console.log(`https://www.bing.com${link2}`)
 
-                console.log(`https://www.bing.com${link2}`)
             }
 
             let results = document.querySelectorAll("#b_results > .b_algo");
